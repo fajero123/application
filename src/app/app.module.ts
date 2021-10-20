ec4ead8255c09ce3a498029b0284e2d464b117c8
fajero123 <acostafediric2001@gmail.com>
2021-10-20 19:47:16 +0800

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+
+
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "**", redirectTo: "home"}
+]
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
